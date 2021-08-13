5728bdd2187ec58dface63e056f6a6dc3785ee03
gtasa3box <74269352+gtasa3box@users.noreply.github.com>
2021-08-13 17:30:46 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,11 +4,41 @@ console.log('global');
 "use strict";
 
 console.log('maxgraph');
+"use strict";
+
 /**
   * название функции
   *
   * @param {number} first - первое число
   * @returns {number}
   */
-"use strict";
+window.onload = function () {
+  document.getElementById('burgerlink').onclick = function () {
+    toogleclass(this);
+    opennav('nav', this);
+    return false;
+  };
+};
+
+function opennav(id, click_burger) {
+  var div = document.getElementById(id);
+
+  if (div.style.display == 'flex') {
+    div.style.display = 'none';
+  } else {
+    div.style.display = 'flex';
+  }
+}
+
+;
+
+function toogleclass(click_burger) {
+  if (click_burger.classList.contains('active')) {
+    click_burger.classList.remove('active');
+  } else {
+    click_burger.classList.add('active');
+  }
+}
+
+;
 //# sourceMappingURL=main.js.map
